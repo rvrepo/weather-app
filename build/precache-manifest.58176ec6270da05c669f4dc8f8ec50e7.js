1dedaedd30d0a025894ca0f9efdfed0d58478608
rvrepo <rony.v12@gmail.com>
2020-05-24 03:54:29 +0530

    adding build

---
+++ build/precache-manifest.58176ec6270da05c669f4dc8f8ec50e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc39603cc7491556e7a24c0bb4b9ebbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65cefa2ab119fc82e9f0",
+    "url": "/static/css/main.57c79cc4.chunk.css"
+  },
+  {
+    "revision": "60a6033a57b59ea9c308",
+    "url": "/static/js/2.5436015b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5436015b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cefa2ab119fc82e9f0",
+    "url": "/static/js/main.94440608.chunk.js"
+  },
+  {
+    "revision": "bbafcfaadf9fe2825757",
+    "url": "/static/js/runtime-main.a58e2989.js"
+  }
+]);
